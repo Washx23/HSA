289d9ed608228223629304b741ad6e2333d5cc7e
Washx23 <5731@holbertonstudents.com>
2023-01-05 05:30:16 -0800

    fix Error

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,10 +12,9 @@ void bubble_sort(int *array, size_t size)
 {
 	int aux;
 	size_t i, j;
-	
+
 	if (array == NULL)
 		return;
-
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - 1; j++)
