9b34583a3e3d0adda2aba5d2c9f9a7cd50de9452
Washx23 <5731@holbertonstudents.com>
2023-01-07 12:41:02 -0800

    sort.h

--- sort.h
+++ sort.h
@@ -18,9 +18,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
